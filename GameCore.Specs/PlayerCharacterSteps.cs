abf5d9dccaacc4fe2fa2b021db279cd85a7f6141
Su <sandar@solidplm.com>
2020-02-21 11:38:01 +0630

    Custom Data Conversions

--- GameCore.Specs/PlayerCharacterSteps.cs
+++ GameCore.Specs/PlayerCharacterSteps.cs
@@ -113,5 +113,17 @@ namespace GameCore.Specs
             Assert.Equal(expectedPower, _player.MagicalPower);
         }
 
+        [Given(@"I last slept (.* days ago)")]
+        public void GivenILastSleptDaysAgo(DateTime lastSlept)
+        {
+            _player.LastSleepTime = lastSlept;
+        }
+
+        [When(@"I read a restore health scroll")]
+        public void WhenIReadARestoreHealthScroll()
+        {
+            _player.ReadHealthScroll();
+        }
+
     }
 }
